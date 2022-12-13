f7ded4ee8f5d6fb2b6e01ab21e4d1fa3840aa8ab
Théo BEAURAIN <tho.beaurain@gmail.com>
2022-12-13 14:45:24 +0100

    update lsv component

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { renderComponent } from "./utils/utils";
 
 // Pages components
 
-import { Header, Footer, Lsv } from "./containers";
-import { Logo, Lottie } from "./components";
+import { Footer } from "./containers";
+import { Logo, Lottie, Lsv } from "./components";
 
 import LogoURL from "./assets/images/logo.png";
 
@@ -14,10 +14,11 @@ import './App.css';
 const App = () => {
     const location = useLocation();
 
-    const [ showLoader, setShowLoader ] = useState(true);    
+    const [ showLoader, setShowLoader ] = useState(true);
 
+    // TODO : Set timer to 3000
     useEffect(() => {
-        setTimeout(() => setShowLoader(false), 5000);
+        setTimeout(() => setShowLoader(false), 3000);
     }, [])
 
     if (showLoader) {
@@ -26,7 +27,7 @@ const App = () => {
                 <div className="logo">
                     <Logo image={LogoURL} />
                 </div>
-                
+
                 <span className="app-presentation">
                     Bienvenue sur LemonTri, une web app pour apprendre les différentes consignes sur le recyclage afin d'adopter un mode de vie plus durable.
                 </span>
@@ -40,11 +41,10 @@ const App = () => {
 
     return (
         <div className="app">
-            <Header />
             <Lsv />
 
             { renderComponent(location && location.pathname) }
-            
+
             <Footer />
         </div>
     );
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,7 +1,5 @@
 // Main containers
-export { default as Header } from "./Header/Header";
 export { default as Footer } from "./Footer/Footer";
 
 // Page containers
 export { default as ListCategories } from "./ListCategories/ListCategories";
-export { default as Lsv } from "./Lsv/Lsv";
