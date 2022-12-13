022907f37c153c5d82ac9f0f7ca3e3d1b0fe4a3b
Théo BEAURAIN <tho.beaurain@gmail.com>
2022-12-13 15:25:20 +0100

    update home

--- src/data/DataCategories.js
+++ src/data/DataCategories.js
@@ -1,25 +1,16 @@
+import Biodechet from "../assets/images/biodechet.png";
+import BottleCan from "../assets/images/bottle-can.png";
+import Can from "../assets/images/can.png";
+import FoldedCarboard from "../assets/images/folded-cardboard.png";
+import PaperCardboard from "../assets/images/paper-cardboard.png";
+
 const dataCategories = (t) => ([
-    { label: t("PaperCup"), color: "#f48033" },
-    { label: t("CigarettesButt"), color: "#545f78" },
-    { label: t("Battery"), color: "#1261ab" },
-    { label: t("Capsule"), color: "#231f20" },
-    // { label: t("Book"), color: Book },
-    { label: t("Glass"), color: "#74bc54" },
-    // { label: t("Reuse"), color: Reuse },
-    { label: t("Paper"), color: "#1261ab" },
-    { label: t("Mask"), color: "#1b8a8b" },
-    { label: t("CoffeeGrounds"), color: "#392810" },
-    // { label: t("Oil"), color: Oil },
-    { label: t("DEEE"), color: "#383b3e" },
-    { label: t("Cartridge"), color: "#03595b" },
-    { label: t("Cardboard"), color: "#af8662" },
-    { label: t("PlasticCup"), color: "#f48033" },
-    { label: t("Can"), color: "#ed423d" },
-    { label: t("Bottle"), color: "#fbb829" },
-    { label: t("BottleCap"), color: "#709aa2" },
-    { label: t("Wood"), color: "#2b1710" },
-    { label: t("Biowaste"), color: "#884f2d" },
-    { label: t("Bulb"), color: "#8e456c" }
+    Biodechet,
+	BottleCan,
+	Can,
+	FoldedCarboard,
+	PaperCardboard,
+	PaperCardboard
 ]);
 
 export default dataCategories;
